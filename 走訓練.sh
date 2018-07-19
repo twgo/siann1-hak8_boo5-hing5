de338bf71b43d0d453fb958733da716bb8ea75f0
leo424y <leo424y@gmail.com>
2018-07-19 08:33:21 +0800

    encode ready

--- 走訓練.sh
+++ 走訓練.sh
@@ -28,94 +28,94 @@ if [ $STAGE -le 1 ]; then
   chmod +x utils/data/perturb_data_dir_encode.sh
   utils/data/perturb_data_dir_encode_alaw_mulaw.sh data/train_guan5 data/train
 fi
-#
-# if [ $STAGE -le 2 ]; then
-#   rm -rf data/lang_train
-#   mkdir -p data/tmp
-#   utils/prepare_lang.sh data/local/dict "<UNK>"  data/tmp/lang_train data/lang_train
-# fi
-#
-# # Now make MFCC features.
-# if [ $STAGE -le 6 ]; then
-#   # mfccdir should be some place with a largish disk where you
-#   # want to store MFCC features.
-#   for i in train; do
-#     data_dir=data/$i
-#     make_mfcc_log=data/mfcc_log/$i
-#     mfccdir=data/mfcc/$i
-#     rm -rf $make_mfcc_log $mfccdir
-#     mkdir -p $make_mfcc_log $mfccdir
-#     utils/fix_data_dir.sh $data_dir
-#     steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" \
-#      $data_dir $make_mfcc_log $mfccdir
-#     steps/compute_cmvn_stats.sh $data_dir $make_mfcc_log $mfccdir
-#   done
-# fi
-#
-# ## Starting basic training on MFCC features
-# if [ $STAGE -le 10 ]; then
-#   steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/mono
-# fi
-#
-# if [ $STAGE -le 11 ]; then
-#   steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/mono exp/mono_ali
-#
-#   steps/train_deltas.sh --cmd "$train_cmd" \
-#     $numLeaves $numGauss data/train data/lang_train exp/mono_ali exp/tri1
-#
-# fi
-#
-# if [ $STAGE -le 12 ]; then
-#   steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/tri1 exp/tri1_ali
-#
-#   steps/train_deltas.sh --cmd "$train_cmd" \
-#     $numLeaves $numGauss data/train data/lang_train exp/tri1_ali exp/tri2
-#
-# fi
-#
-# # From now, we start using all of the data (except some duplicates of common
-# # utterances, which don't really contribute much).
-# if [ $STAGE -le 13 ]; then
-#   steps/align_si.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/tri2 exp/tri2_ali
-#
-#   # Do another iteration of LDA+MLLT training, on all the data.
-#   steps/train_lda_mllt.sh --cmd "$train_cmd" \
-#     $numLeaves $numGauss data/train data/lang_train exp/tri2_ali exp/tri3
-#
-# fi
-#
-# # Train tri4, which is LDA+MLLT+SAT, on all the (nodup) data.
-# if [ $STAGE -le 15 ]; then
-#   steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/tri3 exp/tri3_ali
-#
-#   steps/train_sat.sh  --cmd "$train_cmd" \
-#     $numLeaves $numGauss data/train data/lang_train exp/tri3_ali exp/tri4
-#
-# fi
-#
-# exit 0
-#
-#
-# if [ $STAGE -le 16 ]; then
-#   steps/cleanup/clean_and_segment_data.sh \
-#     --nj $nj \
-#     data/train data/lang_train exp/tri4 exp/tri4_cleanup data/train_cleaned
-#   steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-#     data/train_cleaned data/lang_train exp/tri4 exp/tri4_cleaned_ali
-#   steps/train_sat.sh  --cmd "$train_cmd" \
-#     $numLeaves $numGauss data/train_cleaned data/lang_train exp/tri4_cleaned_ali exp/tri5
-# fi
-#
-# # Prepare tri4_ali for other training
-# if [ $STAGE -le 19 ]; then
-#   steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
-#     data/train data/lang_train exp/tri4 exp/tri4_ali
-# fi
+
+if [ $STAGE -le 2 ]; then
+  rm -rf data/lang_train
+  mkdir -p data/tmp
+  utils/prepare_lang.sh data/local/dict "<UNK>"  data/tmp/lang_train data/lang_train
+fi
+
+# Now make MFCC features.
+if [ $STAGE -le 6 ]; then
+  # mfccdir should be some place with a largish disk where you
+  # want to store MFCC features.
+  for i in train; do
+    data_dir=data/$i
+    make_mfcc_log=data/mfcc_log/$i
+    mfccdir=data/mfcc/$i
+    rm -rf $make_mfcc_log $mfccdir
+    mkdir -p $make_mfcc_log $mfccdir
+    utils/fix_data_dir.sh $data_dir
+    steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" \
+     $data_dir $make_mfcc_log $mfccdir
+    steps/compute_cmvn_stats.sh $data_dir $make_mfcc_log $mfccdir
+  done
+fi
+
+## Starting basic training on MFCC features
+if [ $STAGE -le 10 ]; then
+  steps/train_mono.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/mono
+fi
+
+if [ $STAGE -le 11 ]; then
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/mono exp/mono_ali
+
+  steps/train_deltas.sh --cmd "$train_cmd" \
+    $numLeaves $numGauss data/train data/lang_train exp/mono_ali exp/tri1
+
+fi
+
+if [ $STAGE -le 12 ]; then
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/tri1 exp/tri1_ali
+
+  steps/train_deltas.sh --cmd "$train_cmd" \
+    $numLeaves $numGauss data/train data/lang_train exp/tri1_ali exp/tri2
+
+fi
+
+# From now, we start using all of the data (except some duplicates of common
+# utterances, which don't really contribute much).
+if [ $STAGE -le 13 ]; then
+  steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/tri2 exp/tri2_ali
+
+  # Do another iteration of LDA+MLLT training, on all the data.
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+    $numLeaves $numGauss data/train data/lang_train exp/tri2_ali exp/tri3
+
+fi
+
+# Train tri4, which is LDA+MLLT+SAT, on all the (nodup) data.
+if [ $STAGE -le 15 ]; then
+  steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/tri3 exp/tri3_ali
+
+  steps/train_sat.sh  --cmd "$train_cmd" \
+    $numLeaves $numGauss data/train data/lang_train exp/tri3_ali exp/tri4
+
+fi
+
+exit 0
+
+
+if [ $STAGE -le 16 ]; then
+  steps/cleanup/clean_and_segment_data.sh \
+    --nj $nj \
+    data/train data/lang_train exp/tri4 exp/tri4_cleanup data/train_cleaned
+  steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
+    data/train_cleaned data/lang_train exp/tri4 exp/tri4_cleaned_ali
+  steps/train_sat.sh  --cmd "$train_cmd" \
+    $numLeaves $numGauss data/train_cleaned data/lang_train exp/tri4_cleaned_ali exp/tri5
+fi
+
+# Prepare tri4_ali for other training
+if [ $STAGE -le 19 ]; then
+  steps/align_fmllr.sh --nj $nj --cmd "$train_cmd" \
+    data/train data/lang_train exp/tri4 exp/tri4_ali
+fi
 
 
 # # Dan's nnet recipe
